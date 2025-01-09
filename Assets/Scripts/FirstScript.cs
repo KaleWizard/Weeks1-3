98ef833735e7d766d658e00c339e2dafc705602b
SHERNET\gunterky <kalewizard.dev@gmail.com>
2025-01-09 14:32:37 -0500

    Added a planet and made it move

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x += speed;
+        pos.x %= 10;
+        transform.position = pos;
+    }
+}
